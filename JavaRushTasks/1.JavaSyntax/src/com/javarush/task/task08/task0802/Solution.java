72c5b9938a4ec80cfc5b789e4d2897e5555524f1
rio58g <rio58g@gmail.com>
2017-07-24 23:20:58 +0300

    commit 24.07.2017

--- JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task08/task0802/Solution.java
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task08/task0802/Solution.java
@@ -5,11 +5,32 @@ HashMap из 10 пар
 */
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
 
+        HashMap<String, String> map = new HashMap<String, String>();
+
+        map.put("арбуз", "ягода");
+        map.put("банан", "трава");
+        map.put("вишня", "ягода");
+        map.put("груша", "фрукт");
+        map.put("дыня", "овощ");
+        map.put("ежевика", "куст");
+        map.put("жень-шень", "корень");
+        map.put("земляника", "ягода");
+        map.put("ирис", "цветок");
+        map.put("картофель", "клубень");
+
+        Iterator<Map.Entry<String, String>> iterator = map.entrySet().iterator();
+
+        while(iterator.hasNext()){
+            Map.Entry<String, String> iter = iterator.next();
+            String key = iter.getKey();
+            String value = iter.getValue();
+            System.out.println(key + " - " + value);
+        }
     }
 }
