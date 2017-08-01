b1327f94eab9e3e0d28485238258d634ea7ceefc
rio58g <rio58g@gmail.com>
2017-08-01 23:37:29 +0300

    commit task 01.08.2017

--- JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task08/task0824/Solution.java
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task08/task0824/Solution.java
@@ -4,13 +4,61 @@ package com.javarush.task.task08.task0824;
 Собираем семейство
 */
 
+import java.util.ArrayList;
+
 public class Solution {
     public static void main(String[] args) {
-        //напишите тут ваш код
+
+        Human son1 = new Human("сын Петя", true, 15, new ArrayList<Human>());
+        Human son2 = new Human("сын Вася", true, 21, new ArrayList<Human>());
+        Human daughter = new Human("дочь Маша", false, 18, new ArrayList<Human>());
+
+        ArrayList<Human> ch = new ArrayList<Human>();
+        ch.add(son1);
+        ch.add(son2);
+        ch.add(daughter);
+
+        Human father = new Human("папа Коля", true, 43, ch);
+        Human mother = new Human("мама Света", false, 35, ch);
+
+        ArrayList<Human> fa = new ArrayList<Human>();
+        fa.add(father);
+        ArrayList<Human> mo = new ArrayList<Human>();
+        mo.add(mother);
+
+        Human grandfather1 = new Human("дед Стёпа", true, 80, fa);
+        Human grandmother1 = new Human("бабка Клава", false, 67, fa);
+        Human grandfather2 = new Human("дед Клод", true, 58, mo);
+        Human grandmother2 = new Human("бабка Жозефина", false, 51, mo);
+
+        System.out.println(grandfather1);
+        System.out.println(grandfather2);
+        System.out.println(grandmother1);
+        System.out.println(grandmother2);
+        System.out.println(father);
+        System.out.println(mother);
+        System.out.println(son1);
+        System.out.println(son2);
+        System.out.println(daughter);
+
+
     }
 
     public static class Human {
-        //напишите тут ваш код
+
+        String name;
+        boolean sex;
+        int age;
+        ArrayList<Human> children;
+
+
+        public Human(String name, boolean sex, int age, ArrayList<Human> children) {
+            this.name = name;
+            this.sex = sex;
+            this.age = age;
+            this.children = children;
+
+        }
 
         public String toString() {
             String text = "";
