405a5de84857b77da9bb083d298158ad4576473c
rio58g <rio58g@gmail.com>
2017-08-02 23:12:22 +0300

    commit task 02.08.2017

--- JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task08/task0827/Solution.java
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task08/task0827/Solution.java
@@ -8,10 +8,30 @@ import java.util.Date;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(isDateOdd("MAY 1 2013"));
+        System.out.println(isDateOdd("JANUARY 1 2000"));
+        System.out.println(isDateOdd("JANUARY 2 2020"));
     }
 
     public static boolean isDateOdd(String date) {
-        return true;
+
+        Date d = new Date(date);
+
+        long finish = d.getTime();
+
+        d.setDate(1);
+        d.setMonth(0);
+
+        long start = d.getTime();
+
+        long result = finish - start;
+        long milis = 24*60*60*1000;
+        int resultTime = (int)(result/milis);
+
+        if (resultTime%2 == 0){
+            return true;
+        }
+
+        return false;
     }
 }
+
