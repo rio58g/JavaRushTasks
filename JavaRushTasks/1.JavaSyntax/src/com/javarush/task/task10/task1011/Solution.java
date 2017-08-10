e9bd028a45760ac78132abe5fff3e66846f98236
rio58g <rio58g@gmail.com>
2017-08-10 23:07:41 +0300

    commit task 10.08.2017

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task10/task1011/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task10.task1011;
+
+/* 
+Большая зарплата
+*/
+
+import java.util.ArrayList;
+
+public class Solution {
+
+
+
+
+    public static void main(String[] args) {
+        String s = "Я не хочу изучать Java, я хочу большую зарплату";
+
+
+        for (int i = 0; i < 40; i++){
+           String s1 = s.substring(i, s.length());
+            System.out.println(s1);
+        }
+
+    }
+}
+
