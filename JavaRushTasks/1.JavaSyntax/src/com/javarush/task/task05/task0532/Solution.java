2e20f98f4482fcbecaddd34453d30a37203fcae3
rio58g <rio58g@gmail.com>
2017-08-14 22:52:35 +0300

    commit task 14.08.2017

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task05/task0532/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task05.task0532;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/* 
+Задача по алгоритмам
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int maximum = 0;
+
+        int number = Integer.parseInt(reader.readLine());
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        for (int i = 0; i < number; i++){
+            list.add(Integer.parseInt(reader.readLine()));
+        }
+
+        Collections.sort(list);
+
+        maximum = list.get(list.size()-1);
+
+        System.out.println(maximum);
+    }
+}
