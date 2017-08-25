ccbb9a97db6adbcedda2fcc94992f8a60fb447d5
rio58g <rio58g@gmail.com>
2017-08-25 15:04:37 +0300

    commit task 25.08.2017

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task12/task1215/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task12/task1215/Solution.java
@@ -4,6 +4,9 @@ package com.javarush.task.task12.task1215;
 Кошки не должны быть абстрактными!
 */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Solution {
     public static void main(String[] args) {
 
