abb22ea3c5d87e34d27f388618eb850496c6a7ba
rio58g <rio58g@gmail.com>
2017-08-28 23:01:21 +0300

    commit task 28.08.2017

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task13.task1318;
+
+
+
+/* 
+Чтение файла
+*/
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.IOException;
+/*
+Чтение файла
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException{
+        // напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Scanner scanner = new Scanner(new FileInputStream(reader.readLine()));
+        while (scanner.hasNextLine()) {
+            String data = scanner.nextLine();
+            System.out.println(data);
+        }
+        scanner.close();
+        reader.close();
+    }
+}
---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task13.task1319;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+
+/* 
+Писатель в файл с консоли
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+
+    }
+}
