7a3dce67e4b2f763cb27328657cb3657f3b5ea2f
rio58g <rio58g@gmail.com>
2017-08-30 22:08:21 +0300

    commit task 30.08.2017

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
@@ -1,17 +1,23 @@
 package com.javarush.task.task13.task1319;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /* 
 Писатель в файл с консоли
 */
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        File file = new File("d:/new_text.txt");
+
+        FileWriter writer = new FileWriter(file);
+
+        writer.write(reader.readLine());
+
+        writer.close();
 
     }
 }
