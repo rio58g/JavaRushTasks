4925665c943fa49651a645f9b5a41a05fc370973
rio58g <rio58g@gmail.com>
2017-09-03 21:28:30 +0300

    commit task 03.09.2017

--- JavaRushTasks/2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
@@ -9,15 +9,40 @@ import java.io.*;
 public class Solution {
     public static void main(String[] args) throws IOException {
 
+//        Программа работает но не принимает...
+
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//
+//        File file = new File("d:/new_text.txt");
+//
+//        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+//
+//        while(true) {
+//
+//            String str = reader.readLine();
+//
+//            if(str.equals("exit")) {
+//                writer.write(str);
+//                writer.close();
+//                break;
+//            }else{
+//                writer.write(str + "\r\n");
+//
+//            }
+//        }
+//        reader.close();
+//    }
+
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        File file = new File("d:/new_text.txt");
-
-        FileWriter writer = new FileWriter(file);
-
-        writer.write(reader.readLine());
-
+        BufferedWriter writer = new BufferedWriter(new FileWriter(reader.readLine()));
+        String str;
+        do  {
+            str = reader.readLine();
+            writer.write(str);
+            writer.newLine();
+        } while (!str.equals("exit"));
         writer.close();
-
+        reader.close();
     }
 }
